ca02449cf3ce2b66d2fecb0ba5fe06e5dbb3a118
Thakee Nathees <thakeenathees@gmail.com>
2024-10-08 13:30:25 +0530

    initial commit

---
+++ init.cmd
@@ -0,0 +1,4 @@
+@echo off
+set PYTHONPATH=%PYTHONPATH%;C:\Users\Thakee\Desktop\Home\jaseci\jaseci\jac\jaclang
+set PATH=%PATH%;C:\Users\Thakee\Desktop\Home\jaseci\jactest
+C:\Users\Thakee\Desktop\Home\jaseci\chess\venv\Scripts\activate.bat
